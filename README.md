b065a62512ff5398c709c85c13dd010bbec0e2c6
Thomas Le <thomas.le@parksidelending.com>
2017-04-12 14:59:19 -0700

    Finished initial version of iOS parallax

---
+++ README.md
@@ -0,0 +1,27 @@
+# Simple iOS Parallax Effect
+
+## Example Usage
+
+## Demos
+
+## Installation
+
+* TODO: Add on npm and bower
+
+## License
+
+* TODO: Find appropriate license
+
+## Contribute
+
+If you would like to contribute, please read contributing.md for more info
+
+## Style
+
+We've used [Google ES6 Javascript Style Guide](https://google.github.io/styleguide/jsguide.html)
+
+# TODO
+* How to use
+* Demos
+* Determine browser compatibility
+* Add contributing.md
--- static/js/ios-parallax.js
+++ static/js/ios-parallax.js
@@ -12,18 +12,11 @@ function iosParallax(options){
   $.extend(this.options, options);
 
   /** @private */
-  this.x_center_ = 0;
+  this.centerCoordinates_ = {x: 0, y: 0};
   /** @private */
-  this.y_center_ = 0;
+  this.targetCoordinates_ = {x: 0, y: 0};
   /** @private */
-  this.x_target_ = this.x_center_;
-  /** @private */
-  this.y_target_ = this.y_center_;
-  /** @private */
-  this.x_dampen_ = this.x_center_;
-  /** @private */
-  this.y_dampen_ = this.y_center_;
-
+  this.transitionCoordinates_ = {x: 0, y: 0};
 }
 
 iosParallax.prototype.bindEvents = function(){
@@ -32,16 +25,16 @@ iosParallax.prototype.bindEvents = function(){
   $(self.options.backgroundElement).mousemove(function(e){
     var height = self.options.movementFactor / $(window).height();
     var width = self.options.movementFactor / $(window).width();
-    var pageX = e.pageX - ($(window).width() / 2);
-    var pageY = e.pageY - ($(window).height() / 2);
-    self.x_target_ = width * pageX * -1 + self.x_center_;
-    self.y_target_ = height * pageY * -1 + self.y_center_;
+    var cursorX = e.pageX - ($(window).width() / 2);
+    var cursorY = e.pageY - ($(window).height() / 2);
+    self.targetCoordinates_.x = width * cursorX * -1 + self.centerCoordinates_.x;
+    self.targetCoordinates_.y = height * cursorY * -1 + self.centerCoordinates_.y;
   });
 
   var loop = setInterval(function(){
-    self.x_dampen_ += ((self.x_target_ - self.x_dampen_)/self.options.dampenFactor);
-    self.y_dampen_ += ((self.y_target_ - self.y_dampen_)/self.options.dampenFactor);
-    $(self.options.backgroundElement).css("background-position", self.x_dampen_+"px "+self.y_dampen_+"px");
+    self.transitionCoordinates_.x += ((self.targetCoordinates_.x - self.transitionCoordinates_.x)/self.options.dampenFactor);
+    self.transitionCoordinates_.y += ((self.targetCoordinates_.y - self.transitionCoordinates_.y)/self.options.dampenFactor);
+    $(self.options.backgroundElement).css("background-position", self.transitionCoordinates_.x+"px "+self.transitionCoordinates_.y+"px");
   }, 16);
 
   $(window).resize(function(){
@@ -59,14 +52,13 @@ iosParallax.prototype.bindEvents = function(){
 iosParallax.prototype.setCenteredImageCoordinates_ = function(){
   var self = this;
   var bgImgSize = self.getBackgroundImageSize_();
-  console.log('Background image size', bgImgSize.width, bgImgSize.height);
-  self.x_center_ = -1 * (bgImgSize.width - $(window).width()) / 2;
-  self.y_center_ = -1 *(bgImgSize.height - $(window).height()) / 2;
-  self.x_target_ = self.x_center_;
-  self.y_target_ = self.y_center_;
-  self.x_dampen_ = self.x_center_;
-  self.y_dampen_ = self.y_dampen_;
-}
+  self.centerCoordinates_.x = -1 * (bgImgSize.width - $(window).width()) / 2;
+  self.centerCoordinates_.y = -1 * (bgImgSize.height - $(window).height()) / 2;
+  self.targetCoordinates_.x = self.centerCoordinates_.x;
+  self.targetCoordinates_.y = self.centerCoordinates_.y;
+  self.transitionCoordinates_.x = self.centerCoordinates_.x;
+  self.transitionCoordinates_.y = self.centerCoordinates_.y;
+};
 
 iosParallax.prototype.getBackgroundImageSize_ = function(){
   var self = this;
@@ -74,11 +66,7 @@ iosParallax.prototype.getBackgroundImageSize_ = function(){
   var imgUrl = $(self.options.backgroundElement).css('background-image').replace(/url\(|'|"|'|"|\)$/ig, "");
   img.src = imgUrl;
   return {width: img.width, height: img.height};
-}
-
-iosParallax.prototype.log = function(){
-  console.log('Window width and height', $(window).width(), $(window).height());
-}
+};
 
 
 
